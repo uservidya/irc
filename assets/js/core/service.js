982c12330bfbb33f7766c681d7ad19e2f05192cf
Maythee Anegboonlap <null@llun.in.th>
2013-10-29 23:28:39 +0800

    Finished IRCSpecs.

--- assets/js/core/service.js
+++ assets/js/core/service.js
@@ -1,6 +1,9 @@
 'use strict'
 
 angular.module('Services', [])
+  .factory('Connection', function () {
+    return Primus;
+  })
   .factory('Emitter', function () {
     var Emitter = function () {
 
@@ -18,14 +21,18 @@ angular.module('Services', [])
 
       this.emit = function (name, data) {
         var fns = _events[name];
-        fns.forEach(function (fn) {
-          fn(data);
-        });
+        if (fns) {
+          fns.forEach(function (fn) {
+            fn(data);
+          });
+        }
       }
 
       this.clear = function () {
         _events = {};
       }
+
+      this.id = Math.random();
 
     }
     Emitter.shared = new Emitter;
@@ -59,25 +66,24 @@ angular.module('Services', [])
 
     return Message;
   })
-  .factory('Connection', function () {
-    return Primus;
-  })
   .factory('IRC', [ 'Emitter', 'Message', 'Connection', function (Emitter, Message, Connection) {
     var IRC = function () {
       var _server = '';
       var _user = '';
       var _rooms = [];
-      var _isJoined = false;
       var _nickserv = null;
+
+      this.isJoined = false;
+      var self = this;
      
       var sharedEmitter = Emitter.shared;
       var primus = Connection.connect();
       primus.on('data', function (data) {
         switch (data.action) {
           case 'join':
-            if (!_isJoined) {
+            if (!self.isJoined) {
               _user = data.user;
-              _isJoined = true;
+              self.isJoined = true;
               // Special case for self join. Other command that has user maybe use the same pattern.
               sharedEmitter.emit('self.join', data);
               return;
@@ -110,11 +116,6 @@ angular.module('Services', [])
         get: function () { return _user; }
       });
 
-      this.clear = function () {
-        // Clear all events
-        _events = {};
-      }
-
       this.init = function (server, user, rooms) {
         _server = S(server).trim().s;
         _user = S(user).trim().s;
@@ -126,7 +127,7 @@ angular.module('Services', [])
       }
 
       this.connect = function () {
-        if (this.isInit) {
+        if (self.isInit) {
           // Not support more than 1 room yet.
           var room = _rooms[0];
           primus.write({
@@ -140,10 +141,12 @@ angular.module('Services', [])
       }
 
       this.send = function (text) {
-        var data = (new Message(this.room, text)).data;
-        primus.write(data);
-        if (data.action === 'say') {
-          sharedEmitter.emit('send', { from: _user, room: this.room, message: data.message });
+        if (self.isInit) {
+          var data = (new Message(this.room, text)).data;
+          primus.write(data);
+          if (data.action === 'say') {
+            sharedEmitter.emit('send', { from: _user, room: this.room, message: data.message });
+          }
         }
       }
 
--- specs/client/IRCSpecs.js
+++ specs/client/IRCSpecs.js
@@ -2,30 +2,117 @@
 
 describe('IRC', function () {
 
+  var connectionStub = null;
+
   beforeEach(module('Services'));
   beforeEach(module(function ($provide) {
+    connectionStub = jasmine.createSpyObj('connection', ['write']);
+    connectionStub.map = {};
+    connectionStub.on = function (name, fn) {
+      if (!connectionStub.map[name]) { connectionStub.map[name] = []; }
+      connectionStub.map[name].push(fn);
+    }
+    connectionStub.emit = function (name, data) {
+      var fns = connectionStub.map[name];
+      if (fns) {
+        fns.forEach(function(fn) {
+          fn(data);
+        });
+      }
+    }
+
     $provide.value('Connection', {
       connect: function () {
-        return {
-          on: function () {
-          }
-        }
+        return connectionStub;
       }
     });
   }));
 
   var IRC;
+  var Emitter;
+  var client;
   beforeEach(inject(function ($injector) {
     IRC = $injector.get('IRC');
+    Emitter = $injector.get('Emitter');
+    
+    spyOn(Emitter.shared, 'emit');
+    client = new IRC;
   }));
 
   it ('should have isInit as false when there is no server, nick and room', function () {
-    var client = new IRC;
     expect(client.isInit).toBeFalsy();
   });
 
-  it ('should have isInit as true when init with all values', function () {
-    var client = new IRC;
+  it ('should have room value and isInit should be true after init with all values', function () {
+    client.init('irc.oftc.net', 'nat3', ['#oftc']);
+
+    expect(client.isInit).toBeTruthy();
+    expect(client.room).toEqual('#oftc');
+    expect(client.user).toEqual('nat3');
+  });
+
+  it ('should not connect if connection is not init yet', function () {
+    client.connect();
+
+    expect(connectionStub.write).not.toHaveBeenCalled();
+  });
+
+  it ('should connect with first room', function () {
+    client.init('irc.oftc.net', 'nat3', ['#first', '#second']);
+    client.connect();
+
+    expect(connectionStub.write).toHaveBeenCalledWith({
+      action: 'connect',
+      server: 'irc.oftc.net',
+      room: '#first',
+      user: 'nat3',
+      nickserv: null
+    });
+  });
+
+  it ('should emit event and postdata when backend send data', function () {
+    connectionStub.emit('data', { action: 'event', key: 'value' });
+    expect(Emitter.shared.emit).toHaveBeenCalledWith('event', { action: 'event', key: 'value' });
+    expect(Emitter.shared.emit).toHaveBeenCalledWith('postdata');
+  });
+
+  it ('should change nickname when got nick action with oldname is current user', function () {
+    client.init('irc.oftc.net', 'nat3', ['#first', '#second']);
+    connectionStub.emit('data', { action: 'nick', oldname: 'nat3', newname: 'nat5'});
+    expect(client.user).toEqual('nat5');
+  });
+
+  it ('should not change nickname when got nick action with different user', function () {
+    client.init('irc.oftc.net', 'nat3', ['#first', '#second']);
+    connectionStub.emit('data', { action: 'nick', oldname: 'nat4', newname: 'nat5'});
+    expect(client.user).toEqual('nat3');
+  });
+
+  it ('should change user name when joined and name is conflict', function () {
+    connectionStub.emit('data', { action: 'join', room: '#oftc', user: 'nat4'});
+    expect(client.user).toEqual('nat4');
+    expect(Emitter.shared.emit).toHaveBeenCalledWith('self.join', { action: 'join', room: '#oftc', user: 'nat4'});
+    expect(Emitter.shared.emit).not.toHaveBeenCalledWith('join', { action: 'join', room: '#oftc', user: 'nat4'});
+    expect(Emitter.shared.emit).not.toHaveBeenCalledWith('postdata');
+  });
+
+  it ('should not send data if client is not init', function () {
+    client.send('hello, world');
+    expect(connectionStub.write).not.toHaveBeenCalled();
+  });
+
+  it ('should send data and emit for normal message after init', function () {
+    client.init('irc.oftc.net', 'nat3', ['#tlwg']);
+    client.send('hello, world');
+    expect(connectionStub.write).toHaveBeenCalled();
+    expect(Emitter.shared.emit).toHaveBeenCalledWith('send', { from: 'nat3', room: '#tlwg', message: 'hello, world'});
+  });
+
+  it ('should send data and should not emit for command', function () {
+    client.init('irc.oftc.net', 'nat3', ['#tlwg']);
+    client.send('/nick nat5');
+    expect(connectionStub.write).toHaveBeenCalled();
+    expect(Emitter.shared.emit).not.toHaveBeenCalled();
   });
 
 });
